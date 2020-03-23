35e3c167ecd4701f08f7c7c35316513dd12ae413
Tim Läufer <laeufertim@gmail.com>
2020-03-23 16:05:53 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# MH2-Discord-Bot
+ A custom Discord Bot for playing Monsterhearts 2
---
+++ bot.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 23 16:03:34 2020
+
+@author: Timmitim (Tim Laeufer)
+"""
+
+
