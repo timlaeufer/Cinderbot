29d4f118c03bb7c353504d54233a2e95666a3243
Tim Läufer <laeufertim@gmail.com>
2020-03-24 02:21:44 +0100

    loaded minimal bot example

--- bot.py
+++ bot.py
@@ -6,3 +6,27 @@ Created on Mon Mar 23 16:03:34 2020
 """
 
 
+import discord
+from discord.ext import commands
+
+description = '''An example bot to showcase the discord.ext.commands extension
+module.
+There are a number of utility commands being showcased here.'''
+bot = commands.Bot(command_prefix='.', description=description)
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+
+
+def readToken():
+    with open('token.txt') as f:
+        token = f.readlines()[1]
+    return token
+        
+
+
+await bot.run(readToken())
